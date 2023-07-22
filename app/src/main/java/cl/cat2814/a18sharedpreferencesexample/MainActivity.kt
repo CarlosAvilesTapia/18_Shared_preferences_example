992906e1015f665739387d3a87c26fe34d17698c
CAT <cavilest@gmail.com>
2023-07-21 22:37:48 -0400

    Ejercicio 18 - Implementado view binding. - Se agrega diseño. - Se enlazan las vistas en MainActivity. - Se agrega funcionalidad a los botones. - Se implementa Shared preferences.

--- app/src/main/java/cl/cat2814/a18sharedpreferencesexample/MainActivity.kt
+++ app/src/main/java/cl/cat2814/a18sharedpreferencesexample/MainActivity.kt
@@ -1,12 +1,15 @@
 package cl.cat2814.a18sharedpreferencesexample
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import cl.cat2814.a18sharedpreferencesexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding : ActivityMainBinding
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var mSharedPref: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,17 +17,66 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        mSharedPref = getSharedPreferences("Ind17SharPrefExample", Context.MODE_PRIVATE)
+
         initListeners()
-
-
     }
 
     private fun initListeners() {
-        binding.btSave.setOnClickListener{
+        binding.btSave.setOnClickListener {
             val intNumber = binding.etInt.text.toString().toInt()
             val text = binding.etText.text.toString()
-            val swBool = binding.swBoolean.isChecked.toString().toBoolean()
-            val decNumber = binding.etDecimal.text.toString().toDouble()
+            val swBool = binding.swBoolean.isChecked
+            val decNumber = binding.etDecimal.text.toString().toFloat()
+
+            saveData(intNumber, text, swBool, decNumber)
         }
+
+        binding.btShow.setOnClickListener {
+            showData()
+        }
+
+        binding.btDelete.setOnClickListener {
+            deleteData()
+        }
+    }
+
+    private fun deleteData() {
+
+        binding.tvInt.text = ""
+        binding.tvText.text = ""
+        binding.tvSwitch.text = ""
+        binding.tvDecimal.text = ""
+
+
+        binding.etInt.text.clear()
+        binding.etText.text.clear()
+        binding.swBoolean.isChecked = false
+        binding.etDecimal.text.clear()
+
+        mSharedPref.edit().clear()
+
+    }
+
+    private fun showData() {
+        val intNum = mSharedPref.getInt("My int", 0)
+        val text = mSharedPref.getString("My text", "")
+        val swBool = mSharedPref.getBoolean("My boolean", false)
+        val decNum = mSharedPref.getFloat("My float", 0.0f)
+
+        binding.tvInt.text = intNum.toString()
+        binding.tvText.text = text
+        binding.swBoolean.isChecked = swBool
+        binding.tvSwitch.text = swBool.toString()
+        binding.tvDecimal.text = decNum.toString()
+
+    }
+
+    private fun saveData(intNum: Int, text: String, swBool: Boolean, decNum: Float) {
+        mSharedPref.edit().putInt("My int", intNum).apply()
+        mSharedPref.edit().putString("My text", text).apply()
+        mSharedPref.edit().putBoolean("My boolean", swBool).apply()
+        mSharedPref.edit().putFloat("My float", decNum).apply()
+
     }
 }
